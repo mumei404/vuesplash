2b846e039775a966a3e4a49c8df09776e472e960
shimazaki <lozld30@gmail.com>
2019-07-21 01:39:44 +0900

    set routes

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,7 @@ Route::post('/register', 'Auth\RegisterController@register')->name('register');
 Route::post('/login', 'Auth\LoginController@login')->name('login');
 
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('/user', function(){
+	return Auth::user();
+})->name('user');
