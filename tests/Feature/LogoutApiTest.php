54e9b616a63d15e99954619f9333028a64447535
shimazaki <lozld30@gmail.com>
2019-07-20 13:26:05 +0900

    create logout

---
+++ tests/Feature/LogoutApiTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LogoutApiTest extends TestCase
+{
+	use RefreshDatabase;
+
+	public function setUp()
+	{
+		parent::setUp();
+		$this->user = factory(User::class)->create();
+	}
+
+	public function should_logout()
+	{
+		$response = $this->actingAs($this->user)
+			->json('POST', route('logout'));
+
+		$response->assertStatus(200);
+		$this->assertGuest();
+	}
+}
